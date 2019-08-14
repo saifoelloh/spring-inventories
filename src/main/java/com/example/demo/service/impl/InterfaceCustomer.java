1cb349b72464875717c18cbd6411d5ac869613ef
saifoelloh <saifoelloh@gmail.com>
2019-08-14 22:39:10 +0700

    changing value of interface, because previouse value was DTO

--- src/main/java/com/example/demo/service/impl/InterfaceCustomer.java
+++ src/main/java/com/example/demo/service/impl/InterfaceCustomer.java
@@ -5,41 +5,10 @@ import com.example.demo.entity.Customer;
 import java.util.List;
 import java.util.Optional;
 
-public class InterfaceCustomer {
-    private boolean success;
-    private int code;
-    private String message;
-    private List<Customer> data;
+public interface InterfaceCustomer {
 
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public List<Customer> getData() {
-        return data;
-    }
-
-    public void setData(List<Customer> data) {
-        this.data = data;
-    }
+    List<Customer> findAll();
+    Optional<Customer> findById();
+    boolean addCustomer();
+    boolean removeCustomer();
 }
