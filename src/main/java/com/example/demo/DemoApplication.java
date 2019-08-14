fb15e06eb715890759d1894275ebbd17aa4c95ba
saifoelloh <saifoelloh@gmail.com>
2019-08-14 22:40:47 +0700

    add repository as an variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.entity.Item;
+import com.example.demo.repository.ItemRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +18,18 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Autowired private ItemRepository repository;
+
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
+            repository.save(new Item("Sepatu Adidas", 20, 30, 10, 0));
+            repository.save(new Item("Sepatu Puma", 30, 40, 12, 0));
+            repository.save(new Item("Sepatu", 10, 100, 90, 0));
+            repository.save(new Item("Sepatu", 80, 120, 20, 0));
+            for (Item item: repository.findAll()) {
+                System.out.println(item.toString());
+            }
+        };
+    }
 }
