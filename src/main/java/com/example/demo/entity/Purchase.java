220ffd7397e88c4e4eb992388b1d5d0a9e62e984
saifoelloh <saifoelloh@gmail.com>
2019-08-15 01:23:50 +0700

    changing timestamp, creating toString method, and change discount data type

--- src/main/java/com/example/demo/entity/Purchase.java
+++ src/main/java/com/example/demo/entity/Purchase.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Purchase {
@@ -12,20 +13,49 @@ public class Purchase {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @CreationTimestamp
-    @UpdateTimestamp
     private String id;
-    private Float discount;
+    private float discount;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
 
     @ManyToOne
     @JoinColumn
     private Customer customer;
 
-    public Float getDiscount() {
+    public Purchase() {    }
+
+    public Purchase(float discount) {
+        this.discount = discount;
+    }
+
+    public float getDiscount() {
         return discount;
     }
 
     public void setDiscount(Float discount) {
         this.discount = discount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "id='" + id + '\'' +
+                ", discount=" + discount +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                ", customer=" + customer.getName() +
+                '}';
     }
 }
