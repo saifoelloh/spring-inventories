af5f49f4e22f45b7223ead87028704b8f7c54412
saifoelloh <saifoelloh@gmail.com>
2019-08-15 00:42:27 +0700

    adding service

--- src/main/java/com/example/demo/service/PurchaseService.java
+++ src/main/java/com/example/demo/service/PurchaseService.java
@@ -4,10 +4,12 @@ import com.example.demo.entity.Purchase;
 import com.example.demo.repository.PurchaseRepository;
 import com.example.demo.service.impl.InterfacePurchase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PurchaseService implements InterfacePurchase {
 
     @Autowired
