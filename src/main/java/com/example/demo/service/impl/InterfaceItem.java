a1f989b84b1228641974f395fcb9287126db7057
saifoelloh <saifoelloh@gmail.com>
2019-08-14 22:40:08 +0700

    renaming inteface item

--- src/main/java/com/example/demo/service/impl/InterfaceItem.java
+++ src/main/java/com/example/demo/service/impl/InterfaceItem.java
@@ -1,10 +1,10 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.Item;
 import java.util.List;
 import java.util.Optional;
 
-public interface InterfaceItemService {
+public interface InterfaceItem {
     List<Item> findAll();
     Optional<Item> findById(String id);
     boolean addItem(Item item);
