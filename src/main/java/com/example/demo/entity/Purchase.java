708a84d2e38d61d3d62a7a2c903886f5f42a397f
saifoelloh <saifoelloh@gmail.com>
2019-08-15 15:03:17 +0700

    changing tostring value

--- src/main/java/com/example/demo/entity/Purchase.java
+++ src/main/java/com/example/demo/entity/Purchase.java
@@ -53,8 +53,6 @@ public class Purchase {
         return "Purchase{" +
                 "id='" + id + '\'' +
                 ", discount=" + discount +
-                ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
                 ", customer=" + customer.getName() +
                 '}';
     }
