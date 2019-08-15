c3538e13d604c2cd1552574f2a2b58e5f82c402f
saifoelloh <saifoelloh@gmail.com>
2019-08-15 13:32:19 +0700

    adding getter and setter for relation

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -93,6 +93,14 @@ public class Customer {
         this.discount = discount;
     }
 
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public void setPurchases(List<Purchase> purchases) {
+        this.purchases = purchases;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
